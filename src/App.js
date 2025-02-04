30a7d392a10b7202f8b0f4374b80b930e5b0fde2
Alexandra Pankla <alexandra.pankla@rit.de>
2025-02-04 16:34:47 +0100

    SFKIS-858: React-JS To Do List Übung weiterbearbeitet

--- src/App.js
+++ src/App.js
@@ -20,8 +20,11 @@
 // //     return (<Myfunc></Myfunc>);
 // // }
 
+// import useState from "react";
+import React, {useState} from "react";
+// import TodoApp from "./TodoApp";
+import "./App.css";
 
-import React, { useState } from "react";
 
 const TodoApp = () => {
   const [todo, setTodo] = useState("");
@@ -33,55 +36,36 @@ const TodoApp = () => {
       setTodo("");
     }
   };
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <h1>To Do Liste</h1>
-      <div style={{ marginBottom: "20px" }}>
-        <input
-          type="text"
-          value={todo}
-          onChange={(event) => setTodo(event.target.value)}
-          style={{
-            padding: "10px",
-            width: "200px",
-            marginRight: "10px",
-            borderRadius: "4px",
-            border: "1px solid #ccc",
-          }}
-        />
-        <button
-          onClick={addTodo}
-          style={{
-            padding: "10px 20px",
-            borderRadius: "4px",
-            border: "none",
-            backgroundColor: "#007bff",
-            color: "#fff",
-            cursor: "pointer",
-          }}
-        >
-          Hinzufügen
-        </button>
-      </div>
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        {todoList.map((item, index) => (
-          <li
-            key={index}
-            style={{
-              background: "#f9f9f9",
-              margin: "5px 0",
-              padding: "10px",
-              borderRadius: "4px",
-              border: "1px solid #ddd",
-            }}
+    
+        return (
+      <div id="main">
+        <h1>To Do Liste</h1>
+        <div id="textinput">
+          <input
+            type="text"
+            value={todo}
+            onChange={(event) => setTodo(event.target.value)}
+          />
+          <button
+            onClick={addTodo}
           >
-            {item}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+            Hinzufügen
+          </button>
+        </div>
+        <ul>
+          {todoList.map((item, index) => (
+            <li
+              key={index}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
 
 export default TodoApp;
+
+// NXT: To Do List in eigene Klassen wie vorher!
